dd40b38f1fd62a92436e50d8becf92ad2c73636b
Rose-mkt <rose.mkt@gmail.com>
2024-08-21 15:02:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ y = df['species']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 st.sidebar.title('Classification')
-classifier = st.sidebar.selectbox('Classifier', ('KNN', 'Decision Tree', 'Random Forest', 'SVM'))
+classifier = st.sidebar.selectbox('Select classifier', ('KNN', 'DT', 'RF', 'SVM', 'NN'))
 k = st.sidebar.slider('K', 1, 120, 3)
 from sklearn.metrics import accuracy_score
 y_pred = knn.predict(x_test)
@@ -36,4 +36,10 @@ from sklearn.ensemble import RandomForestClassifier
 rf = RandomForestClassifier()
 rf.fit(x_train, y_train)
 y_pred = rf.predict(x_test)
+accuracy_score(y_test, y_pred)
+
+from sklearn.neural_network import MLPClassifier
+nn = MLPClassifier()
+nn.fit(x_train, y_train)
+y_pred = nn.predict(x_test)
 accuracy_score(y_test, y_pred)
