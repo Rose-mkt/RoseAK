c11d3d1bcebe88907dc794b50a05b2b339050d9d
Rose-mkt <rose.mkt@gmail.com>
2024-08-21 14:45:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ import streamlit as st
 import seaborn as sns
 df = sns.load_dataset('iris')
 df
+
+x = df.iloc[:, :-1]
+y = df['species']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+from sklearn.metrics import accuracy_score
+y_pred = knn.predict(x_test)
+accuracy_score(y_test, y_pred)
+
+from sklearn.svm import SVC
+svm = SVC()
+svm.fit(x_train, y_train)
+y_pred = svm.predict(x_test)
+accuracy_score(y_test, y_pred)
